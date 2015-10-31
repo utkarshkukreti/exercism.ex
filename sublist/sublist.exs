4794a4ee0b00d5d991ca92782b19eaf5c58cf318
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-10-31 18:23:20 +0530

    solve sublist

--- sublist/sublist.exs
+++ sublist/sublist.exs
@@ -4,6 +4,22 @@ defmodule Sublist do
   and if not whether it is equal or unequal to the second list.
   """
   def compare(a, b) do
-
+    {la, lb} = {length(a), length(b)}
+    cond do
+      la == lb && a == b -> :equal
+      la < lb && sublist?(a, b) -> :sublist
+      la > lb && sublist?(b, a) -> :superlist
+      true -> :unequal
+    end
   end
+
+  defp sublist?([], _), do: true
+  defp sublist?(_, []), do: false
+  defp sublist?(a, [_ | t] = b) do
+    starts_with?(b, a) || sublist?(a, t)
+  end
+
+  defp starts_with?(_, []), do: true
+  defp starts_with?([h | ta], [h | tb]), do: starts_with?(ta, tb)
+  defp starts_with?(_, _), do: false
 end
