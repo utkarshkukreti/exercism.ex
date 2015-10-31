db397f40730768073c0b871e20293d10ac4410c5
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-10-31 17:53:36 +0530

    solve word-count

--- word-count/word_count.exs
+++ word-count/word_count.exs
@@ -6,6 +6,11 @@ defmodule Words do
   """
   @spec count(String.t) :: map()
   def count(sentence) do
-
+    Regex.scan(~r/(\p{L}|\p{N}|-)+/u, sentence)
+    |> Enum.map(&hd/1)
+    |> Enum.map(&String.downcase/1)
+    |> Enum.reduce(%{}, fn word, acc ->
+      Map.update(acc, word, 1, &(&1 + 1))
+    end)
   end
 end
