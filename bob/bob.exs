f8a92c93d5cbb05b140f6c6af95f0562bc510f10
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-10-31 17:51:12 +0530

    solve bob

--- bob/bob.exs
+++ bob/bob.exs
@@ -1,8 +1,14 @@
 defmodule Teenager do
   def hey(input) do
     cond do
-        true -> raise "Your implementation goes here"
-
+      String.ends_with?(input, "?") ->
+        "Sure."
+      input == String.upcase(input) && input != String.downcase(input) ->
+        "Whoa, chill out!"
+      String.strip(input) == "" ->
+        "Fine. Be that way!"
+      true ->
+        "Whatever."
     end
   end
 end
