61db7f6ca19e6bb1ac6054eda9b87646c96f6e64
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-11-01 11:19:10 +0530

    solve leap

--- leap/leap.exs
+++ leap/leap.exs
@@ -10,6 +10,6 @@ defmodule Year do
   """
   @spec leap_year?(non_neg_integer) :: boolean
   def leap_year?(year) do
-  
+    rem(year, 4) == 0 && (rem(year, 100) != 0 || rem(year, 400) == 0)
   end
 end
