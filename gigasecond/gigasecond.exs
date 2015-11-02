70294d0c67e02bd565e8187d9507afa08fedcfde
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-11-02 14:31:31 +0530

    solve gigasecond

--- gigasecond/gigasecond.exs
+++ gigasecond/gigasecond.exs
@@ -1,10 +1,13 @@
 defmodule Gigasecond do
-	@doc """
-	Calculate a date one billion seconds after an input date.
-	"""
-	@spec from({pos_integer, pos_integer, pos_integer}) :: :calendar.date
-
-	def from({year, month, day}) do
-
-	end
+  @doc """
+  Calculate a date one billion seconds after an input date.
+  """
+  @spec from({pos_integer, pos_integer, pos_integer}) :: :calendar.date
+  def from({year, month, day}) do
+    {date, time} =
+      :calendar.datetime_to_gregorian_seconds({{year, month, day}, {0, 0, 0}})
+      |> +(1_000_000_000)
+      |> :calendar.gregorian_seconds_to_datetime
+    date
+  end
 end
--- gigasecond/gigasecond_test.exs
+++ gigasecond/gigasecond_test.exs
@@ -5,27 +5,22 @@ else
 end
 
 ExUnit.start
-ExUnit.configure exclude: :pending
 
 defmodule GigasecondTest do
   use ExUnit.Case
 
-  # @tag :pending
   test "from 4/25/2011" do
     assert Gigasecond.from({2011, 4, 25}) == {2043, 1, 1}
   end
 
-  @tag :pending
   test "from 6/13/1977" do
     assert Gigasecond.from({1977, 6, 13}) == {2009, 2, 19}
   end
 
-  @tag :pending
   test "from 7/19/1959" do
     assert Gigasecond.from({1959, 7, 19}) == {1991, 3, 27}
   end
 
-  @tag :pending
   test "yourself" do
     # customize these values for yourself
     # your_birthday = {year1, month1, day1}
