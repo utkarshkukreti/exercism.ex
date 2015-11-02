79ad7a07452e3dd877236542cf62bda5a6b3e925
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-11-02 11:46:36 +0530

    solve space-age

--- space-age/space_age.exs
+++ space-age/space_age.exs
@@ -8,6 +8,15 @@ defmodule SpaceAge do
   """
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
-
+    seconds / orbital_period(planet)
   end
+
+  def orbital_period(:earth),   do: 31557600
+  def orbital_period(:mercury), do: orbital_period(:earth) * 0.2408467
+  def orbital_period(:venus),   do: orbital_period(:earth) * 0.61519726
+  def orbital_period(:mars),    do: orbital_period(:earth) * 1.8808158
+  def orbital_period(:jupiter), do: orbital_period(:earth) * 11.862615
+  def orbital_period(:saturn),  do: orbital_period(:earth) * 29.447498
+  def orbital_period(:uranus),  do: orbital_period(:earth) * 84.016846
+  def orbital_period(:neptune), do: orbital_period(:earth) * 164.79132
 end
