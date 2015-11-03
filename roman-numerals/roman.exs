d415f8bbb897ed316543a40531a970ebfbf0fbd2
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-11-03 15:14:32 +0530

    solve roman-numerals

--- roman-numerals/roman.exs
+++ roman-numerals/roman.exs
@@ -1,9 +1,26 @@
 defmodule Roman do
+  @table [{"M", 1000},
+          {"CM", 900},
+          {"D", 500},
+          {"CD", 400},
+          {"C", 100},
+          {"XC", 90},
+          {"L", 50},
+          {"XL", 40},
+          {"X", 10},
+          {"IX", 9},
+          {"V", 5},
+          {"IV", 4},
+          {"I", 1}]
+
   @doc """
   Convert the number to a roman number.
   """
   @spec numerals(pos_integer) :: String.t
-  def numerals(number) do
-  
+  def numerals(0), do: ""
+  for {string, number} <- @table do
+    def numerals(n) when n >= unquote(number) do
+      unquote(string) <> numerals(n - unquote(number))
+    end
   end
 end
