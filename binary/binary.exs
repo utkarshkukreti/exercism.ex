1db079dda64d2018c878aae4cbff5b18e00b9b7c
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-11-03 15:31:43 +0530

    solve binary

--- binary/binary.exs
+++ binary/binary.exs
@@ -6,6 +6,12 @@ defmodule Binary do
   """
   @spec to_decimal(String.t) :: non_neg_integer
   def to_decimal(string) do
-  
+    string
+    |> String.to_char_list
+    |> Enum.reduce(0, fn
+      ?0, acc -> acc * 2
+      ?1, acc -> acc * 2 + 1
+      _,  _   -> 0
+    end)
   end
 end
