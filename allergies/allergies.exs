51ac4b2176f22b8571890d87798f2db3cb87fc95
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-11-05 16:41:31 +0530

    solve allergies

--- allergies/allergies.exs
+++ allergies/allergies.exs
@@ -1,10 +1,22 @@
 defmodule Allergies do
+  use Bitwise
+
+  @allergies %{"eggs" => 1,
+               "peanuts" => 2,
+               "shellfish" => 4,
+               "strawberries" => 8,
+               "tomatoes" => 16,
+               "chocolate" => 32,
+               "pollen" => 64,
+               "cats" => 128}
+
   @doc """
   List the allergies for which the corresponding flag bit is true.
   """
   @spec list(non_neg_integer) :: [String.t]
   def list(flags) do
-  
+    Map.keys(@allergies)
+    |> Enum.filter(&allergic_to?(flags, &1))
   end
 
   @doc """
@@ -12,6 +24,6 @@ defmodule Allergies do
   """
   @spec allergic_to?(non_neg_integer, String.t) :: boolean
   def allergic_to?(flags, item) do
-  
+    (@allergies[item] &&& flags) > 0
   end
 end
