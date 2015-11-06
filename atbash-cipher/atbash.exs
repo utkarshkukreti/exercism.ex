7c094db43b6d1f5ff2214e37edc9ec8b1bfcacbc
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-11-06 17:07:55 +0530

    solve atbash-cipher

--- atbash-cipher/atbash.exs
+++ atbash-cipher/atbash.exs
@@ -9,6 +9,22 @@ defmodule Atbash do
   """
   @spec encode(String.t) :: String.t
   def encode(plaintext) do
-  
+    plaintext
+    |> String.downcase
+    |> String.to_char_list
+    |> Enum.map(&encode_char/1)
+    |> Enum.reject(&is_nil/1)
+    |> Enum.chunk(5, 5, [])
+    |> Enum.map(&List.to_string/1)
+    |> Enum.join(" ")
   end
+
+  @alphabet 'abcdefghijklmnopqrstuvwxyz'
+  for {a, b} <- Enum.zip(@alphabet, Enum.reverse(@alphabet)) do
+    defp encode_char(unquote(a)), do: unquote(b)
+  end
+  for a <- '0123456789' do
+    defp encode_char(unquote(a)), do: unquote(a)
+  end
+  defp encode_char(_), do: nil
 end
