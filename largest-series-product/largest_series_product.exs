f24bfbc85e58951cea4fc88f929c4c03438afede
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-11-09 20:15:33 +0530

    solve largest-series-product

--- largest-series-product/largest_series_product.exs
+++ largest-series-product/largest_series_product.exs
@@ -5,7 +5,9 @@ defmodule Series do
   """
   @spec digits(String.t) :: [non_neg_integer]
   def digits(number_string) do
-    
+    number_string
+    |> String.to_char_list
+    |> Enum.map(&(&1 - ?0))
   end
 
   @doc """
@@ -13,14 +15,20 @@ defmodule Series do
   """
   @spec slices(String.t, non_neg_integer) :: [list(non_neg_integer)]
   def slices(number_string, size) do
-
+    number_string
+    |> digits
+    |> Enum.chunk(size, 1)
   end
 
   @doc """
   Finds the largest product of a given number of consecutive numbers in a given string of numbers.
   """
   @spec largest_product(String.t, non_neg_integer) :: non_neg_integer
+  def largest_product(_, 0), do: 1
   def largest_product(number_string, size) do
-   
+    number_string
+    |> slices(size)
+    |> Enum.map(&Enum.reduce(&1, 1, fn a, b -> a * b end))
+    |> Enum.max
   end
 end
