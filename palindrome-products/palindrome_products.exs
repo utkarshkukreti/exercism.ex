a4d26e578199be6b11336e00fbf6a460490bc462
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-11-12 20:43:06 +0530

    solve palindrome-products

--- palindrome-products/palindrome_products.exs
+++ palindrome-products/palindrome_products.exs
@@ -5,6 +5,17 @@ defmodule Palindromes do
   """
   @spec generate(non_neg_integer, non_neg_integer) :: map() 
   def generate(max_factor, min_factor \\ 1) do
-    
+    for(a <- min_factor..max_factor,
+        b <- a..max_factor,
+        palindrome?(a * b)) do
+      [a, b]
+    end
+    |> Enum.group_by(fn [a, b] -> a * b end)
+    |> Enum.map(fn {key, value} -> {key, Enum.reverse(value)} end)
+  end
+
+  defp palindrome?(number) do
+    string = Integer.to_string(number)
+    string == String.reverse(string)
   end
 end
